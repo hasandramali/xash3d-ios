8e89f3ff77e325bacf7d4f0551b384353a901e1d
hasandramali <93338836+hasandramali@users.noreply.github.com>
2024-08-07 17:03:16 +0300

    Update fetch-projects.sh

--- scripts/fetch-projects.sh
+++ scripts/fetch-projects.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-git clone https://github.com/FWGS/xash3d --depth 1
-git clone https://github.com/FWGS/nanogl --depth 1
+git clone https://github.com/hasandramali/xash3d --recursive
+git clone https://github.com/tyabus/nanogl --depth 1
 git clone https://github.com/FWGS/microndk --depth 1
 wget http://libsdl.org/release/SDL2-2.0.5.tar.gz
 tar xzf SDL2-2.0.5.tar.gz
